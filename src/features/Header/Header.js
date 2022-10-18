6fc042bbf4a1c5853f8484e7a838eb988548839e
bippity-boppity <94451050+bippity-boppity@users.noreply.github.com>
2022-10-18 11:31:13 +0100

    Added initial structure

---
+++ src/features/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+  return (
+    <>
+      <p>Header</p>
+    </>
+  )
+};
+
+export default Header;
---
+++ src/features/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+      <p>Home</p>
+    </>
+  )
+};
+
+export default Home;
